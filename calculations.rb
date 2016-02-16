62aff472cc039b4b51e6bffdd8c4bf50ff579450
Cory Logan <clogan@cloudpassage.com>
2016-02-15 20:17:39 -0800

    added weight calculation

--- calculations.rb
+++ calculations.rb
@@ -7,10 +7,16 @@ Bundler.require(:default)
 @item_list = []
 
 class Item
-  attr_accessor :price, :name, :length, :quantity
+  attr_accessor :price, :name, :length, :quantity, :weight
 
   def price=(number)
     @price = number.round(2)
+  end
+
+  def weight=(number)
+    @weight = number.round(1)
+  rescue
+    @weight = 0
   end
 end
 
@@ -20,6 +26,7 @@ def add_simple_item(name)
     item = Item.new
     item.price = object["price"]
     item.name = name
+    item.weight = object["weight"]
     @item_list << item
   end
 end
@@ -30,6 +37,12 @@ add_simple_item("single_socket_tee")
 add_simple_item("extra_heavy_flange")
 
 
+
+def weight_of_pipe_by(length)
+  unit_weight = @properties["pipe"]["weight"]
+  (length/12) * unit_weight
+end
+
 def price_of_pipe_by(length)
   unit_price = @properties["pipe"]["price"]
   (length/12) * unit_price
@@ -38,10 +51,11 @@ end
 def add_pipe_item(name, length)
   object = @properties[name]
   object["quantity"].times do
-    item = Item.new
-    item.price = price_of_pipe_by(length)
+    item        = Item.new
+    item.price  = price_of_pipe_by(length)
+    item.weight = weight_of_pipe_by(length)
     item.length = "#{length}\""
-    item.name = "#{name} pipe"
+    item.name   = "#{name} pipe"
     @item_list << item
   end
 end
@@ -79,6 +93,10 @@ add_pipe_item("short_crosspiece", calculate_short_crosspiece_length)
 
 tp @item_list
 
-prices = @item_list.map {|item| item.price}
-total_cost = prices.inject(0, :+)
-puts "Total cost: $#{total_cost.round(2)}"
+def sum(column)
+  prices = @item_list.map {|item| item.send(column)}
+  prices.inject(0, :+)
+end
+
+puts "Total cost: $#{sum("price").round(2)}"
+puts "Total weight: #{sum("weight").round(1)} lbs"
